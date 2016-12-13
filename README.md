46e0d2c67a81b7ffdcf9d62b1c4ffa5d5b125de5
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2016-12-13 17:28:59 +0530

    add popup screen center window organize files

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 A chrome extension that lets you share text, image, link from any page to your Twitter profile and also shows you the sentiment of your tweet before posting.
 
 ## Installing unpacked extension
+Clone/download the repository.  
 Goto ``` chrome://extensions ``` (or menu >  More Tools > Extensions).  
 Enable Developer mode on the checkbox in the upper-right corner.  
 Click on the "Load unpacked extension" button.  
 Select the directory containing unpacked extension.
 
-### How to use it?
+## How to use it?
 Right click on whatever you want to share and select the Tweet Sweet option on the menu.
---
+++ js/background.js
@@ -0,0 +1,40 @@
+var windowWidth = 600;
+var windowHeight = 600;
+var windowLeft = (screen.width/2)-(windowWidth/2);
+var windowTop = (screen.height/2)-(windowHeight/2);
+
+var contextList = ["selection", "link", "image", "page"];
+var twitterWebIntentURL = "https://twitter.com/intent/tweet";
+
+for(i = 0; i < contextList.length; i++) {
+	var curContext = contextList[i];
+	var curTitle = "Tweet Sweet: Share this " + curContext + " on your Twitter profile.";
+	
+	chrome.contextMenus.create({
+		title: curTitle,
+		contexts: [curContext],
+		onclick: tweetFunction,
+		id: curContext
+	});
+}
+
+function tweetFunction(data, tab) {
+	switch(data.menuItemId) {
+		case 'selection' :
+			chrome.windows.create({url: twitterWebIntentURL + "?text=" + encodeURIComponent(data.selectionText), 
+			type: "panel", width: windowWidth, height: windowHeight, left: windowLeft, top: windowTop}, function(window) {});
+			break;
+		case 'link' :
+			chrome.windows.create({url: twitterWebIntentURL + "?url=" + encodeURIComponent(data.linkUrl), 
+			type: "panel", width: windowWidth, height: windowHeight, left: windowLeft, top: windowTop}, function(window) {});
+			break;
+		case 'image' :
+			chrome.windows.create({url: twitterWebIntentURL + "?url=" + encodeURIComponent(data.srcUrl), 
+			type: "panel", width: windowWidth, height: windowHeight, left: windowLeft, top: windowTop}, function(window) {});
+			break;
+		case 'page' :
+			chrome.windows.create({url: twitterWebIntentURL + "?text=" + encodeURIComponent(tab.title) + "&url=" + encodeURIComponent(data.pageUrl), 
+			type: "panel", width: windowWidth, height: windowHeight, left: windowLeft, top: windowTop}, function(window) {});
+			break;
+	}
+}
