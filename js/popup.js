e5835b0caf0b89297fd6da82689b992cb1e87083
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2016-12-13 18:55:39 +0530

    update on extension icon click

--- js/popup.js
+++
@@ -1,8 +0,0 @@
-var windowWidth = 600;
-var windowHeight = 600;
-var windowLeft = (screen.width/2)-(windowWidth/2);
-var windowTop = (screen.height/2)-(windowHeight/2); 
-var twitterWebIntentURL = "https://twitter.com/intent/tweet";
-
-chrome.windows.create({url: twitterWebIntentURL, type: "panel", width: windowWidth, height: windowHeight, left: windowLeft, top: windowTop}, 
-	function(window) {});
